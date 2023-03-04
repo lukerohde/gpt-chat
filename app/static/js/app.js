85576ccb8bc3225317133477398116d904b4a8e3
LUKE ROHDE <lukerohde@gmail.com>
2023-03-04 23:22:24 +1100

    ws finally working in prod behind nginx with daphne

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -91,7 +91,7 @@ $(document).ready(function () {
 
 //    let socket = new WebSocket(`ws://127.0.0.1:8000/?session_key=${sessionKey}`);
     var socket = new WebSocket(
-        'ws://' + window.location.host +
+        'wss://' + window.location.host +
         '/ws?session_key=${sessionKey}')
 
     chatInput.keypress(function (e) {
